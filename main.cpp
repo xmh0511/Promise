46c7c8db6b29f88c5469344449c335f0a07f39ec
xmh0511 <970252187@qq.com>
2019-02-13 17:18:11 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,35 +2,47 @@
 #include "promise.hpp"
 #include <chrono>
 #include <thread>
+
+template<typename Function>
+auto setTimeOut(Function&& function,int time)
+{
+	auto point = std::chrono::steady_clock::now();
+	std::shared_ptr<std::thread> t = std::make_shared<std::thread>([function, point, time]() mutable {
+		while (true) {
+			auto now = std::chrono::steady_clock::now();
+			auto distance = now - point;
+			auto t = std::chrono::duration_cast<std::chrono::duration<long, std::ratio<1, 1>>>(distance).count();
+			//std::cout << t << std::endl;
+			if (t > time) {
+				function();
+				break;
+			}
+		}
+	});
+	t->detach();
+	return t;
+}
 int main()
 {
 	 auto c = promise_co<>().then([](){
 		auto promise = promise_co<int, double>::create();
-		//promise.resolve(20, 20.12);
-		auto point = std::chrono::steady_clock::now();
-		std::thread t([promise, point]() mutable {
-			while (true) {
-				auto now = std::chrono::steady_clock::now();
-				auto distance = now - point;
-				auto t = std::chrono::duration_cast<std::chrono::duration<long, std::ratio<1, 1>>>(distance).count();
-				//std::cout << t << std::endl;
-				if (t > 5) {
-					break;
-				}
-			}
+		setTimeOut([promise]() mutable {
 			promise.resolve(20, 20.12);
-		});
-		t.detach();
+		}, 5);
 		return promise;
 	 }).then([](int a, double b) {
 		auto promise = promise_co<std::string,int>::create();
 		std::cout << a << "  " << b << std::endl;
-		promise.resolve(std::string("OK"), 20);
+		setTimeOut([promise]() mutable {
+			promise.resolve(std::string("OK"), 20);
+		}, 2);
 		return promise;
 	}).then([](auto str,auto i) {
 		auto promise = promise_co<double,std::string>::create();
 		std::cout << str << "   " << i << std::endl;
-		promise.resolve(12.03, std::string("end"));
+		setTimeOut([promise]() mutable {
+			promise.resolve(12.03, std::string("end"));
+		}, 3);
 		return promise;
 	});
 
