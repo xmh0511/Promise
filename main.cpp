4592531176e927e247db323abcb3814802b2970a
xmh0511 <970252187@qq.com>
2019-02-13 17:10:31 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include "promise.hpp"
+#include <chrono>
+#include <thread>
 int main()
 {
-	auto c = promise_co().then([](auto promise){
-	    return promise->resolve(20,20.12);
-	}).then([](auto promise,int a,double b) {
-		std::cout << a <<"   "<< b << std::endl;
-		return promise->resolve(std::string("OK"),20);
-	}).then([](auto promise, auto str,auto i) {
+	 auto c = promise_co<>().then([](){
+		auto promise = promise_co<int, double>::create();
+		//promise.resolve(20, 20.12);
+		auto point = std::chrono::steady_clock::now();
+		std::thread t([promise, point]() mutable {
+			while (true) {
+				auto now = std::chrono::steady_clock::now();
+				auto distance = now - point;
+				auto t = std::chrono::duration_cast<std::chrono::duration<long, std::ratio<1, 1>>>(distance).count();
+				//std::cout << t << std::endl;
+				if (t > 5) {
+					break;
+				}
+			}
+			promise.resolve(20, 20.12);
+		});
+		t.detach();
+		return promise;
+	 }).then([](int a, double b) {
+		auto promise = promise_co<std::string,int>::create();
+		std::cout << a << "  " << b << std::endl;
+		promise.resolve(std::string("OK"), 20);
+		return promise;
+	}).then([](auto str,auto i) {
+		auto promise = promise_co<double,std::string>::create();
 		std::cout << str << "   " << i << std::endl;
-		return promise->resolve(std::string("OK"), 20);
+		promise.resolve(12.03, std::string("end"));
+		return promise;
 	});
 
-	c.then([](auto promise,auto str,auto b) {
+	c.then([](auto str,auto b) {
+		auto promise = promise_co<>::create();
 		std::cout << str << "   " << b << std::endl;
-		return promise->resolve();
+		return promise;
 	});
-
 
 	std::cin.get();
 	return 0;
--- promise.hpp
+++ promise.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <tuple>
 #include <string>
+#include <memory>
 template<typename...Args>
 class Promise;
 
@@ -17,16 +18,22 @@ struct is_promise_type<Promise<Args...>>
 	static constexpr bool value = true;
 };
 
-template<typename...Args>
-struct is_promise_type<Promise<Args...>&>
-{
-	static constexpr bool value = true;
-};
+//template<typename...Args>
+//struct is_promise_type<Promise<Args...>&>
+//{
+//	static constexpr bool value = true;
+//};
+//
+//template<typename...Args>
+//struct is_promise_type<Promise<Args...>&&>
+//{
+//	static constexpr bool value = true;
+//};
 
 template<typename...Args>
-struct is_promise_type<Promise<Args...>&&>
+struct context
 {
-	static constexpr bool value = true;
+
 };
 
 template<typename...Args>
@@ -66,9 +73,9 @@ public:
 	Promise() = default;
 
 	template<typename...Params,typename  = std::enable_if_t<has_no_promise<Params...>::value>>
-	Promise(Params&&...params):args_tuple(std::forward<Params>(params)...)
+	Promise(Params&&...params) //:args_tuple(std::forward<Params>(params)...)
 	{
-
+		std::cout << typeid(context<Params...>).name() << std::endl;
 	}
 
 	Promise(Promise const& p):args_tuple(p.args_tuple)
@@ -90,30 +97,84 @@ public:
 	template<typename Function>
 	auto then(Function&& function)
 	{
-		auto cb =  then_call(std::forward<Function>(function), std::make_index_sequence<sizeof...(Args)>{});
 		while (status_ == promise_state::pending)
 		{
 
 		}
+		auto cb =  then_call(std::forward<Function>(function), std::make_index_sequence<sizeof...(Args)>{});
 		return cb;
 	}
 public:
 	template<typename...Params>
-	auto resolve(Params&&...args)->Promise<Params...>
+	auto resolve(Params&&...args)
 	{
+		args_tuple = std::tuple<Args...>(args...);
 		status_ = promise_state::fulfilled;
-		return Promise<Params...>(std::forward<Params>(args)...);
+	}
+
+	static auto create()
+	{
+		return std::make_shared<Promise>();
 	}
 private:
 	template<typename Function,std::size_t...Indexs>
 	auto then_call(Function&& function,std::index_sequence<Indexs...>)
 	{
-		return function(this,std::get<Indexs>(args_tuple)...);
+		return function(std::get<Indexs>(args_tuple)...);
 	}
 
 private:
 	std::tuple<Args...> args_tuple;
 	promise_state status_ = promise_state::pending;
+};
+
+template<>
+class Promise<> :public PromiseBase
+{
+public:
+	Promise() = default;
+public:
+	template<typename Function>
+	auto then(Function&& function)
+	{
+		auto cb = function();
+		return cb;
+	}
+	static auto create()
+	{
+		return std::make_shared<Promise>();
+	}
+};
+
+template<typename...Args>
+class promise_co
+{
+public:
+	promise_co() = default;
+
+	promise_co(promise_co const& p):co_(p.co_)
+	{
+
+	}
+public:
+	template<typename Function>
+	auto then(Function&& function)
+	{
+		return co_->then(std::forward<Function>(function));
+	}
+
+	template<typename...Params>
+	auto resolve(Params&&...args)
+	{
+		return co_->resolve(std::forward<Params>(args)...);
+	}
+
+	static auto create()
+	{
+		return promise_co{};
+	}
+private:
+	std::shared_ptr<Promise<Args...>> co_ = std::make_shared<Promise<Args...>>();
 };
 
 template<typename...Args>
@@ -122,4 +183,3 @@ auto call_back(Args&&...args)->Promise<Args...>
 	return Promise<Args...>(std::forward<Args>(args)...);
 }
 
-using promise_co = Promise<>;
