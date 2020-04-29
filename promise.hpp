3d4892689227862460c48953f2cc809d130ed372
xmh0511 <970252187@qq.com>
2020-04-29 17:20:55 +0800

    Update promise.hpp

--- promise.hpp
+++ promise.hpp
@@ -1,109 +1,142 @@
 #pragma once
-#include <thread>
-#include <condition_variable>
 #include <tuple>
 #include <functional>
-#include <mutex>
+#include <future>
 #include <memory>
-#include <string>
 namespace xmh {
-	template<typename...Args>
-	class promise;
-	template<typename...Args>
-	class promise_co
-	{
-	public:
-		promise_co() {
-			promise_ = std::make_shared<promise<Args...>>();
-		}
-		template<typename...Params>
-		void resolve(Params&&...params) const {
-			promise_->resolve(std::forward<Params>(params)...);
-		}
-		template<typename Lambda>
-		auto then(Lambda&& function) const {
-			return promise_->then(std::forward<Lambda>(function));
-		}
-	private:
-		mutable std::shared_ptr<promise<Args...>> promise_;
+	template<typename T, typename U = void>
+	struct function_traits;
+	template<typename Ret, typename...Args>
+	struct function_traits<Ret(*)(Args...)> {
+		using ret_type = Ret;
+		using parameters_type = std::tuple<Args...>;
+		using non_reference_params_type = std::tuple<typename std::remove_reference<Args>::type...>;
+		static constexpr std::size_t args_size = sizeof...(Args);
 	};
-
-	enum  class promise_state :std::uint32_t
-	{
-		init,
-		pending,
-		resolve,
-		reject,
+	template<typename Ret, typename Class, typename...Args>
+	struct function_traits<Ret(Class::*)(Args...) const> {
+		using ret_type = Ret;
+		using parameters_type = std::tuple<Args...>;
+		using non_reference_params_type = std::tuple<typename std::remove_reference<Args>::type...>;
+		static constexpr std::size_t args_size = sizeof...(Args);
 	};
+	template<typename T>
+	struct function_traits<T, std::void_t<decltype(&T::operator())>> :function_traits<decltype(&T::operator())> {
 
-	template<typename...Args>
-	class promise
-	{
-		//template<typename T>
-		//friend class promise_co<T>;
-	public:
-		promise() {
-			state_ = promise_state::init;
-		}
-	public:
-		template<typename...Params>
-		void resolve(Params&&...params) {
-			//std::cout << typeid(arguments_).name() << "   " << sizeof...(params)<<"   "<<typeid(tp).name() << std::endl;
-			arguments_ = std::tuple<Args...>(static_cast<Args>(params)...);
-			resolvet_ = std::thread([that = this]() mutable {
-				std::unique_lock<std::mutex> lck(that->other_mutex_);
-				that->other_cv_.wait(lck,[that]() {
-					return that->state_ == promise_state::pending;
-				});
-				if (that->state_ == promise_state::pending) {
-					that->state_ = promise_state::resolve;
-					that->cv.notify_one();
-				}
-			});
-			resolvet_.detach();
-		}
-		template<typename Lambda>
-		auto then(Lambda&& function) {
-			std::unique_lock<std::mutex> lck(mutex_);
-			{
-				std::unique_lock<std::mutex> lck0(other_mutex_);
-				state_ = promise_state::pending;
-			}
-			other_cv_.notify_one();
-			cv.wait(lck, [this]() {
-				return this->state_ == promise_state::resolve;
-			});
-			return apply(std::forward<Lambda>(function), std::make_index_sequence<sizeof...(Args)>{});
-		}
-	protected:
-		template<std::size_t...Indexs, typename Lambda>
-		auto apply(Lambda&& function, std::index_sequence<Indexs...>) {
-			return function(std::get<Indexs>(arguments_)...);
-		}
-	private:
-		std::tuple<Args...> arguments_;
-		std::mutex mutex_;
-		std::condition_variable cv;
-		promise_state state_;
-		std::thread resolvet_;
-		std::mutex other_mutex_;
-		std::condition_variable other_cv_;
 	};
+	struct any_data_hold {
+		virtual ~any_data_hold() {
 
-	template<>
-	class promise<>
-	{
-	public:
-		promise() = default;
-		template<typename Lambda>
-		auto then(Lambda&& function) {
-			auto pm = function();
-			return pm;
 		}
 	};
+	template<typename T>
+	struct any_data :any_data_hold {
+		template<typename U>
+		any_data(U&& v) :value_(v) {
 
-	promise<> get_promise() {
-		return promise<>{};
+		}
+		T value_;
+	};
+	struct any {
+		any() = default;
+		template<typename T, typename U = typename std::enable_if<!std::is_same<any, typename std::remove_reference<T>::type>::value>::type>
+		any(T&& value) :holder_(std::make_shared<any_data<typename std::remove_reference<T>::type>>(std::forward<T>(value))) {
+
+		}
+		std::shared_ptr<any_data_hold> holder_;
+	};
+
+	template<typename T>
+	T any_cast(any const& v) {
+		auto r = std::dynamic_pointer_cast<any_data<T>>(v.holder_);
+		if (r) {
+			return r->value_;
+		}
+		throw T{};
 	}
+
+
+	template<typename T>
+	struct Resolver {
+		Resolver(T* that) :that_(that) {
+
+		}
+		template<typename...U>
+		void operator()(U&& ...value) const {
+			if (that_) {
+				that_->resolve(std::forward<U>(value)...);
+			}
+		}
+	private:
+		T* that_ = nullptr;
+	};
+	template<typename T>
+	struct Rejecter {
+		Rejecter(T* that) :that_(that) {
+
+		}
+		template<typename...U>
+		void operator()(U&& ...value) const {
+			if (that_) {
+				that_->reject(std::forward<U>(value)...);
+			}
+		}
+	private:
+		T* that_ = nullptr;
+	};
+
+	struct promise_implementation {
+		promise_implementation() = default;
+		template<typename Task>
+		promise_implementation(Task&& task) :task_snapshot_(std::bind(std::forward<Task>(task), Resolver<promise_implementation>{ this }, Rejecter<promise_implementation>{this})) {
+
+		}
+
+		template<typename...Args>
+		void resolve(Args&& ...args) {
+			auto tp = std::make_tuple(args...);
+			promise_.set_value(tp);
+		}
+
+		template<typename...Args>
+		void reject(Args&& ...args) {
+
+		}
+
+		template<typename Function, typename Tuple, std::size_t...Indexs>
+		auto deference(Function&& exectuor, Tuple&& tup, std::index_sequence<Indexs...>) {
+			return exectuor(std::get<Indexs>(tup)...);
+		}
+
+		template<typename Function>
+		auto then(Function&& exectuor) {
+			task_snapshot_();
+			auto future = promise_.get_future();
+			auto v = future.get();
+			using traits = function_traits<typename std::remove_reference<Function>::type>;
+			using params_type = typename traits::non_reference_params_type;
+			return deference(std::forward<Function>(exectuor), any_cast<params_type>(v), std::make_index_sequence<traits::args_size>{});
+		}
+
+	private:
+		std::function<void()> task_snapshot_;
+		std::promise<any> promise_;
+	};
+	class Promise {
+	public:
+		Promise() = default;
+		Promise(Promise const& p) :promise_(p.promise_) {
+
+		}
+		template<typename Task, typename U = typename std::enable_if<!std::is_same<std::remove_reference_t<Task>, Promise>::value>::type>
+		Promise(Task&& task) : promise_(std::make_shared<promise_implementation>(std::forward<Task>(task))) {
+		}
+	public:
+		template<typename Function>
+		auto then(Function&& exectuor) {
+			return promise_->then(std::forward<Function>(exectuor));
+		}
+	private:
+		std::shared_ptr<promise_implementation> promise_;
+	};
 }
-#define GO(...) std::thread([]{ __VA_ARGS__ }).detach();
